cc8851bad490a3670f73244995923984149d32d6
ShilpiMaurya <meshilpi94@gmail.com>
2021-09-22 15:29:51 +0530

    added: firebase sdk

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,10 +1,40 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+const { setCookie } = require("nookies");
+const admin = require("firebase-admin");
+const serviceAccount = process.env.NEXT_PUBLIC_FIREBASE_SERVICE_ACCOUNT;
+
+if (!admin.apps.length) {
+  if (serviceAccount) {
+    admin.initializeApp({
+      credential: admin.credential.cert(JSON.parse(serviceAccount))
+    });
+  } else {
+    console.error("Cant read or access serviceAccount");
+  }
+}
+
+const auth = admin.auth();
+
 export default async function loginHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req) {
-    res.status(200).json({ name: "shilpi" });
+  try {
+    const { method } = req;
+    const { email, password } = req.body;
+
+    if (method === "POST") {
+      const info = await auth.signInWithEmailAndPassword({
+        email,
+        password
+      });
+      console.log(info);
+      res.status(200);
+    } else {
+      res.status(405).send("Method not allowed");
+    }
+  } catch (error) {
+    res.status(500).send({ message: `${error.code} - ${error.message}` });
   }
 }
