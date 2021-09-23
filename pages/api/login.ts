5e35f894bcce95551bdf2f0cfafeae60d3d50ac7
ShilpiMaurya <meshilpi94@gmail.com>
2021-09-23 16:19:39 +0530

    added: firebase/auth to login existing user

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,8 +1,23 @@
 import { NextApiRequest, NextApiResponse } from "next";
-
-const { setCookie } = require("nookies");
+require("firebase/auth");
+const firebase = require("firebase");
 const admin = require("firebase-admin");
 const serviceAccount = process.env.NEXT_PUBLIC_FIREBASE_SERVICE_ACCOUNT;
+const { setCookie } = require("nookies");
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 if (!admin.apps.length) {
   if (serviceAccount) {
@@ -14,23 +29,27 @@ if (!admin.apps.length) {
   }
 }
 
-const auth = admin.auth();
-
 export default async function loginHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   try {
     const { method } = req;
-    const { email, password } = req.body;
+    const { email, password, checked } = req.body;
 
     if (method === "POST") {
-      const info = await auth.signInWithEmailAndPassword({
-        email,
-        password
-      });
-      console.log(info);
-      res.status(200);
+      const uniqueUserId = await firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password);
+
+      if (checked === true) {
+        setCookie({ res }, "uid", `${uniqueUserId.user.uid}`, {
+          maxAge: 30 * 24 * 60 * 60,
+          httpOnly: true,
+          path: "/"
+        });
+      }
+      res.status(200).json({ uid: uniqueUserId.user.uid });
     } else {
       res.status(405).send("Method not allowed");
     }
