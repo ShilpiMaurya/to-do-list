b428c01ccf5b654d843ed0c310ec16957a6e0be6
ShilpiMaurya <meshilpi94@gmail.com>
2021-10-15 15:02:35 +0530

    refactored: tasks.ts

--- pages/api/tasks.ts
+++ pages/api/tasks.ts
@@ -23,48 +23,54 @@ export default async function handler(
     const { body, method } = req;
     const parsedCookies = parseCookies({ req });
     const uidCookie = parsedCookies.uid;
-    if (method === "POST" && uidCookie) {
-      const {
-        taskTitle,
-        description,
-        startDate,
-        endDate,
-        status,
-        priority
-      } = body;
 
-      const docRef = await db.collection(`${uidCookie}`).add({
-        taskTitle,
-        description,
-        startDate,
-        endDate,
-        status,
-        priority
-      });
-      res.status(200).json({ uniqueId: docRef.id });
-    } else if (method === "GET" && uidCookie) {
-      let taskList: Array<{
-        endDate: string;
-        status: string;
-        priority: string;
-        description: string;
-        startDate: string;
-        taskTitle: string;
-      }> = [];
-      const tasks = db.collection(`${uidCookie}`);
-      const snapshot = await tasks.get();
-      snapshot.forEach((doc: any) => {
-        taskList.push(doc.data());
-      });
-      res.status(200).json(taskList);
-    } else if (
-      (method === "GET" && !uidCookie) ||
-      (method === "POST" && !uidCookie)
-    ) {
-      res.status(401).send("Unauthorized Request");
-    } else {
-      res.status(405).send("Method not allowed");
+    if (method === "POST") {
+      if (uidCookie) {
+        const {
+          taskTitle,
+          description,
+          startDate,
+          endDate,
+          status,
+          priority
+        } = body;
+
+        const docRef = await db.collection(`${uidCookie}`).add({
+          taskTitle,
+          description,
+          startDate,
+          endDate,
+          status,
+          priority
+        });
+        res.status(200).json({ uniqueId: docRef.id });
+      } else {
+        res.status(401).send("Unauthorized Request");
+      }
     }
+
+    if (method === "GET") {
+      if (uidCookie) {
+        let taskList: Array<{
+          endDate: string;
+          status: string;
+          priority: string;
+          description: string;
+          startDate: string;
+          taskTitle: string;
+        }> = [];
+
+        const tasks = db.collection(`${uidCookie}`);
+        const snapshot = await tasks.get();
+        snapshot.forEach((doc: any) => {
+          taskList.push(doc.data());
+        });
+        res.status(200).json(taskList);
+      } else {
+        res.status(401).send("Unauthorized Request");
+      }
+    }
+    res.status(405).send("Method not allowed");
   } catch (error) {
     res.status(500).send({ message: `${error.code} - ${error.message}` });
   }
